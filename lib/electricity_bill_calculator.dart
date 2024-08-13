b54a81a3f2201b0af13bdafcb521ef96ac44596a
Waqas Muhammad <139709619+WaqasMuhammad5254@users.noreply.github.com>
2024-08-13 19:13:18 +0500

    Dart Simple Proram

---
+++ lib/electricity_bill_calculator.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+void main() {
+  /*
+  write a program to calculate elctricity bill
+  cost per unit varies upto 20-50
+  if consumed unit is lessthan 100 than cost per unit is 20
+  if consumed unit is greater than 100 but lessthan 200 than cost per unit is 30
+  if consumed unit is grater 200 than cost per unit is 50
+   */
+
+  int consumedUnits, costPerUnits, totalBill;
+
+  stdout.write('Enter consumed units: ');
+  consumedUnits = int.parse(stdin.readLineSync()!);
+  costPerUnits = 0;
+
+  if (consumedUnits >= 0 && consumedUnits <= 100) {
+    totalBill = consumedUnits * 20;
+    print('Your electricity bill is Rs. $totalBill');
+  } else if (consumedUnits > 100 && consumedUnits <= 200) {
+    totalBill = consumedUnits * 30;
+    print('Your electricity bill is Rs. $totalBill');
+  } else {
+    totalBill = consumedUnits * 50;
+    print('Your electricity bill is Rs. $totalBill');
+  }
+}

88d8ecc0bd627c21a92782bdc3068b5db69a3ebf
Waqas Muhammad <waqasmuhammad5254@gmail.com>
2025-01-12 01:28:47 +0500

    Leet Code Problem

--- lib/leet_code_series/two_sum.dart
+++ lib/leet_code_series/two_sum.dart
@@ -1,1 +1,20 @@
-class TwoSum {}
+import 'dart:io';
+
+class TwoSum {
+  void twoSum() {
+    int num1, num2, sum;
+    stdout.write('Enter First Number: ');
+    num1 = int.parse(stdin.readLineSync()!);
+    stdout.write('Enter Second Number: ');
+    num2 = int.parse(stdin.readLineSync()!);
+
+    sum = num1 + num2;
+
+    print('Sum of $num1 and $num2 = $sum');
+  }
+}
+
+void main() {
+  TwoSum twoSum = TwoSum();
+  twoSum.twoSum();
+}

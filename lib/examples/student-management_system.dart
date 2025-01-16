48c05362d1b743544f9e470f86ff7c3c9b56354a
Waqas Muhammad <waqasmuhammad5254@gmail.com>
2025-01-16 12:43:04 +0500

    Dart Example

---
+++ lib/examples/student-management_system.dart
@@ -0,0 +1,15 @@
+void main() {}
+
+class Student {
+  String? name;
+  int? age;
+  int? id;
+
+  Student(
+    this.name,
+    this.id,
+    this.age,
+  );
+
+  void addStudent(Student student) {}
+}
